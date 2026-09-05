d1804f3e60b1a3de941344ffeeee4d32ab10678e
Rochiit Sharma <156521118+Rochiit-Sharma@users.noreply.github.com>
2026-09-05 11:40:59 +0530

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,11 @@
+package student;
+
+public class StudentDetails {
+
+    public void displayStudent() {
+        System.out.println("Student Details:");
+        System.out.println("Name: Rochiit");
+        System.out.println("Roll No: 101");
+        System.out.println("Course: Computer Engineering");
+    }
+}
